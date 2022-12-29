610fa51de22444ec27c2de3c52f75137af2c65d5
Jhonny Reis <118661994+devjhonnyreis@users.noreply.github.com>
2022-12-28 21:13:47 -0300

    atualização

--- Exercicios/ex010/exercicio/script.js
+++ Exercicios/ex010/exercicio/script.js
@@ -1,19 +1,22 @@
 function carregar() {
     var txt = document.getElementById('hora')
     var foto = document.getElementById('foto')
+    var pe = document.getElementById('pe')
     var data = new Date()
-    // var hora = data.getHours()
-    var hora = 20
+    var hora = data.getHours()
+    
 
     if (hora >= 4 && hora < 12) {
-        
         foto.style.backgroundImage = "url(imagens/foto-manha.png)"
+        txt.innerHTML = `Bom dia, agora são ${hora} horas.`
     } else if (hora >= 12 && hora < 19) {
         document.body.style.background = '#be5f3c'
         foto.style.backgroundImage = "url(imagens/foto-tarde.png)"
+        txt.innerHTML = `Boa tarde, agora são ${hora} horas.`
     } else {
         foto.style.backgroundImage = "url(imagens/foto-noite.png)"
         document.body.style.background = 'black'
-        document.querySelector('footer').color = 'white'
+        pe.style.color = '#F3F3F3'
+        txt.innerHTML = `Boa noite, agora são ${hora} horas.`
     }
 }
