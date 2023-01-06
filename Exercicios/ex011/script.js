60427b340353a103b1f423982cb513ad16257f30
Jhonny Reis <118661994+devjhonnyreis@users.noreply.github.com>
2023-01-06 09:56:00 -0300

    atualização

--- Exercicios/ex011/script.js
+++ Exercicios/ex011/script.js
@@ -3,11 +3,32 @@ function cont(){
     var fim = document.getElementById('ifim')
     var passo = document.getElementById('ipass')
     var res = document.getElementById('res')
-    var um = num.value
-    var dois = fim.value
-    var tres = passo.value
+    
+    if (num.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO] Dados inválidos, preencha corretamente!')
+        res.innerHTML = `Impossível contar!`
+    } else {
+        var um = Number(num.value)
+        var dois = Number(fim.value)
+        var tres = Number(passo.value)
 
-    for ( var i = um; i <= dois; i + tres) {
-        res.innerText = `${i}`
+        res.innerHTML = 'Contando...'
+
+    if (tres <= 0) {
+        tres = 1
+        window.alert('Passo inválido, considerando passo 1')
+    }
+
+    if (um < dois){
+        for (var c = um; c <= dois; c += tres) {
+            res.innerHTML += `${c}, \u{1F449} `
+        }    
+    
+    } else {
+        for (var c = um; c >= dois; c -= tres) {
+            res.innerHTML += `${c}, \u{1F449} `
+        }
+    }
+    res.innerHTML += `Fim!`
     }
 }
